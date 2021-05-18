4a815e9084be631374ff47f46339c2b9081a40a9
Александр <spider_inter@mail.ru>
2021-05-18 16:50:58 +0300

    hw

--- src/ru/GeekBrains/Lesson6/AdvancedOop/Cat.java
+++ src/ru/GeekBrains/Lesson6/AdvancedOop/Cat.java
@@ -1,4 +1,4 @@
-package ru.GeekBrains.AdvancedOop;
+package ru.GeekBrains.Lesson6.AdvancedOop;
 
 public class Cat extends Animals {
 
--- src/ru/GeekBrains/Lesson6/AdvancedOop/Main.java
+++ src/ru/GeekBrains/Lesson6/AdvancedOop/Main.java
@@ -1,4 +1,4 @@
-package ru.GeekBrains.AdvancedOop;
+package ru.GeekBrains.Lesson6.AdvancedOop;
 
 public class Main {
     public static void main(String[] args) {
